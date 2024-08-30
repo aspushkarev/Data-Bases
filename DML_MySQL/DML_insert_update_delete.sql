1fa1b222f38d765b5692d44a621a8fd97c403380
Alexander_Pushkarev <aspushkarev@icloud.com>
2024-08-30 20:33:31 +0300

    added some code

--- DML_MySQL/DML_insert_update_delete.sql
+++ DML_MySQL/DML_insert_update_delete.sql
@@ -9,7 +9,7 @@ INNER JOIN issuers i ON i.id = d.issuer_id
 WHERE d.closed = 'no'
 ORDER BY u.id;
 
--- Запрос для отображения подробной информации по эмитенту из портфеля инвестора
+-- Запрос для отображения подробной информации из портфеля инвестора, у которого сделка не закрыта
 
 SELECT 	u.firstname, 
 		u.lastname, 
@@ -18,14 +18,14 @@ SELECT 	u.firstname,
 		d.quantity, 								 -- quantity - количество ценных бумаг
 		i.price,									 -- price - текущая цена на рынке
 		d.price AS book_price,						 -- book_price - балансовая цена
-		(SELECT d.quantity * i.price) AS value, 	 -- value - стоимость
-		(SELECT d.quantity * d.price) AS book_value, -- book_value - балансовая стоимость
+		d.quantity * i.price AS value, 			 	 -- value - стоимость
+		d.quantity * d.price AS book_value, 		 -- book_value - балансовая стоимость
 		(SELECT book_value - value) AS revaluation	 -- revaluation - переоценка (за всё время)
 FROM issuers i  
 LEFT JOIN deals d ON i.id = d.issuer_id
 LEFT JOIN users u ON u.id = d.user_id
-WHERE d.closed = 'no' AND u.id = 4
-ORDER BY u.id;
+WHERE d.closed = 'no' AND u.id = 1
+ORDER BY d.quantity;
 
 
 -- Запрос показывающий состояние счёта инвестора
@@ -35,19 +35,16 @@ WHERE bou.user_id = 1
 GROUP BY currency_id 
 ORDER BY currency_id;
 
--- Запрос показывающий аббревиатуру валюты и её символ
-
-SELECT c.abbreviation, c.symbol FROM currencys c
-WHERE c.id = 1;
-
 -- Запрос с подзапросами показывающий состояние счёта инвестора
 
 SELECT
 	bou.user_id,
 	SUM(bou.cash) AS cash,
-	(SELECT c.symbol FROM currencys c WHERE c.id = bou.currency_id) AS symbol,
-	(SELECT c2.abbreviation FROM currencys c2 WHERE c2.id = bou.currency_id) AS abbreviation
-FROM bill_of_users bou WHERE bou.user_id = 1
+	c.symbol,
+	c.abbreviation 
+FROM bill_of_users bou 
+JOIN currencys c ON c.id = bou.currency_id 
+WHERE bou.user_id = 1
 GROUP BY currency_id 
 ORDER BY currency_id;
 
@@ -56,9 +53,40 @@ ORDER BY currency_id;
 SELECT CONCAT(u.firstname, ' ', u.lastname, ' ', u.patronymic) AS name, 
 	i.name_of_the_issuer, 
 	d.deal, 
-	d.quantity, 
+	d.quantity,
 	d.price, 
-	(SELECT d.quantity * d.price) AS book_value FROM deals d
+	d.quantity * d.price AS book_value 
+FROM deals d
 INNER JOIN issuers i ON d.issuer_id = i.id
 INNER JOIN users u ON u.id = d.user_id 
 WHERE u.id = 3;
+
+-- Запрос показывающий аббревиатуру валюты и её символ
+
+SELECT abbreviation, symbol FROM currencys
+WHERE id = 1;
+
+-- Запрос показывающий квалифицированных инвесторов 
+-- (счёт по требованиям ЦБ должен быть больше 6 млн. ₽)
+
+SELECT CONCAT(u.firstname, ' ', u.lastname, ' ', u.patronymic) AS name,
+	cash FROM bill_of_users bou
+JOIN users u ON bou.user_id = u.id
+WHERE cash > 6000000;
+
+-- Запрос показывающий клиентов, у которых счёт близок к нулю (возможен margin call)
+
+SELECT CONCAT(u.firstname, ' ', u.lastname, ' ', u.patronymic) AS name,
+	cash FROM bill_of_users bou
+JOIN users u ON bou.user_id = u.id
+WHERE cash < 1000;
+
+-- Запрос показывающий облигации с доходностью более 7%
+
+SELECT name_of_the_issuer, info_about_issuer, profitability FROM bounds
+WHERE profitability > 7;
+
+-- Запрос показывающий клиентов, зарегистрированных после 1 июня 2024 года
+
+SELECT * FROM users
+WHERE created_at > '2024-06-01';
